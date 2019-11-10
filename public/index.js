60b9ddcd85af2ae02e27bf93cdbd0711cc646d3c
Wellington.BT <w.b.teodoro@gmail.com>
2019-11-10 20:19:43 -0300

    Commit inicial

---
+++ public/index.js
@@ -0,0 +1,100 @@
+
+
+
+function adicionaItem(){
+    let addInfo = {};
+    let databaseRef = firebase.database().ref();
+
+    const temperatura = $('#input-temperatura').val();
+    const umidade = $('#input-umidade').val();
+    const velocidadeVento = $('#input-velocidade').val();
+
+    const newId = databaseRef.child('dados').push().key;
+
+    if(temperatura && umidade && velocidadeVento){
+
+        addInfo['/dados/' + newId] = {
+            temperatura,
+            umidade,
+            velocidadeVento
+        };
+        
+        databaseRef.update(addInfo)
+            .then(() => {
+                return true
+            })
+            .catch((err) => {
+                return err
+            })
+
+        $('#input-temperatura').val('');
+        $('#input-umidade').val('');
+        $('#input-velocidade').val('');
+    }
+
+}
+
+async function listaDados() {
+    let databaseRef = firebase.database().ref();
+    const dados = databaseRef.child('dados');
+
+    let tbody = document.getElementById('corpoDados');
+    let ultimaTemperatura = document.getElementById('ultimaTemperatura')
+    let ultimaUmidade = document.getElementById('ultimaUmidade')
+    let ultimaVelocidade = document.getElementById('ultimaVelocidade')
+
+    dados.on("value", snap => {
+        let dadosColetados = snap.val();
+        dadosColetados = Object.values(dadosColetados);
+
+        while (tbody.firstChild) {
+            tbody.removeChild(tbody.firstChild);
+        }
+
+        ultimaTemperatura.textContent = dadosColetados[dadosColetados.length - 1].temperatura + ' ºC';
+        ultimaUmidade.textContent = dadosColetados[dadosColetados.length - 1].umidade + '%';
+        ultimaVelocidade.textContent = dadosColetados[dadosColetados.length - 1].velocidadeVento  + ' km/h';
+
+        dadosColetados.map((item, index) => {
+            
+            let trDados = document.createElement('tr');
+            trDados.setAttribute('id', 'dado' + index + 1);
+            tbody.appendChild(trDados);
+            
+            let thDadoIndex = document.createElement('th');
+            let tdDadoTemperatura = document.createElement('td');
+            let tdDadoUmidade = document.createElement('td');
+            let tdDadoVelocidade = document.createElement('td');
+
+            thDadoIndex.textContent = index + 1;
+            tdDadoVelocidade.textContent = item.velocidadeVento + ' km/h';
+            tdDadoUmidade.textContent = item.umidade + '%';
+            tdDadoTemperatura.textContent = item.temperatura + ' ºC';
+            
+            trDados.appendChild(thDadoIndex);
+            trDados.appendChild(tdDadoVelocidade);
+            trDados.appendChild(tdDadoUmidade);
+            trDados.appendChild(tdDadoTemperatura);
+        })
+
+    });
+}
+
+
+
+// async function atualizaLista(){
+//     alert('asd')
+//     const itemId = databaseRef.child('dados').push().key;
+
+//     let databaseRef = firebase.database().ref('dados/');
+//     let lista = [];
+
+//     databaseRef.on('value') 
+//         .then((snapshot) => {
+//             console.log(snapshot.val());
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//         })
+
+// }   
---
+++ public/js/firebase.js
@@ -0,0 +1,15 @@
+(function (){
+    var firebaseConfig = {
+        apiKey: "AIzaSyDNfDOY8wzUbL087kssbK1HWOIK7imuMPI",
+        authDomain: "teste-comdados2019.firebaseapp.com",
+        databaseURL: "https://teste-comdados2019.firebaseio.com",
+        projectId: "teste-comdados2019",
+        storageBucket: "teste-comdados2019.appspot.com",
+        messagingSenderId: "773792944556",
+        appId: "1:773792944556:web:bda90397341a0d30e02a09",
+        measurementId: "G-T3ZDW69TPR" 
+      };
+  
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+})()
